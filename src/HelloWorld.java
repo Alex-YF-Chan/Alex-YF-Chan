90e9726485c3f8a74816a9ab152366d99edc86e6
DESKTOP-3S9NH3M\m3zo0 <alexfeiwork@gmail.com>
2021-12-19 14:37:00 +0800

    edit 輸入文字

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("請輸入文字");
+		String s1 = scanner.next();
+		System.out.println("---------");
+		System.out.println(s1);
 		scanner.close();
 
 	}
